856e1524d79b911e5c9202b1adbb620c25fb42a7
Krishna <104274041+KrishnaSampathkumar@users.noreply.github.com>
2023-04-25 21:54:36 +0200

    Create app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-import 
+import scholarly
+
+search_query = scholarly.search_pubs('retracted:yes')
+for i in range(10):
+    result = next(search_query)
+    if 'retraction' in result.bib:
+        print(result.bib['title'])
+        print(result.bib['retraction']['reason'])
